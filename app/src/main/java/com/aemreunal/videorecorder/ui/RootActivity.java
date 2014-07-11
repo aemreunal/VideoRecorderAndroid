9d8db0c2b99d9706436ec8c79fdb3d96e8ef3f3c
A. Emre Ünal <aemreunal@gmail.com>
2014-07-11 15:54:11 +0300

    Initial commit.

---
+++ app/src/main/java/com/aemreunal/videorecorder/ui/RootActivity.java
@@ -0,0 +1,70 @@
+package com.aemreunal.videorecorder.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.aemreunal.videorecorder.R;
+
+
+public class RootActivity extends Activity {
+
+    public static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private EditText textField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+        textField = (EditText) findViewById(R.id.editText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.root, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void recordButtonTapped(View view) {
+        dispatchTakeVideoIntent();
+    }
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        int duration = Integer.parseInt(textField.getText().toString());
+        takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, duration);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            Toast.makeText(this, "Video recorded. URL: " + videoUri, Toast.LENGTH_LONG).show();
+        }
+    }
+}
